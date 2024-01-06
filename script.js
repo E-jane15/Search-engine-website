b31658cef15778ec195177da58427595691a1e7c
E-jane15 <janeebaimay@gmail.com>
2024-01-07 00:19:59 +0100

    Linked Unsplash API to project.

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+const accessKey = "NfYUE5LXZ08YOvzDod6pUCaavG_uU9ysxVKyZglGiKw";
+
 const searchForm = document.getElementById("search-form");
 const searchBox = document.getElementById("search-box");
 const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
+
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+    keyword = searchBox.ariaValueMax;
+    const url ='https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+
+
+}
