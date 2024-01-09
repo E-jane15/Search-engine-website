60c9056d8fb1bdfbdf2d66bf2d8114c77eac20e6
E-jane15 <janeebaimay@gmail.com>
2024-01-09 03:30:24 +0100

    Connecting the links of the images on api to script.js

--- script.js
+++ script.js
@@ -10,12 +10,19 @@ let keyword = "";
 let page = 1;
 
 async function searchImages(){
-    keyword = searchBox.ariaValueMax;
-    const url ='https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
-
-const response = await fetch(url);
+    keyword = searchBox.value;
+    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+const response = await axios.get(url);
 const data = await response.json();
-console.log(data);
+
+const results = data.results;
+results.map((result) => {
+   const image = document.createElement("img");
+   image.src = result.urls.small;
+   const imageLink =document.createElement("a");
+   imageLink.href = result.links.html;
+   imageLink.target = "_blank"
+})
 }
 
 searchForm.addEventListener("submit",(e) =>{
