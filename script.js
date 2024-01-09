eeea93ce53391bf9dd9ab2f353dd7f3a48c64e50
E-jane15 <janeebaimay@gmail.com>
2024-01-09 04:10:01 +0100

    Added code to hide previous results when a new keyword is entered.

--- script.js
+++ script.js
@@ -12,8 +12,12 @@ let page = 1;
 async function searchImages(){
     keyword = searchBox.value;
     const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12';
-const response = await axios.get(url);
-const data = response.data;
+const response = await fetch(url);
+const data = await response.json();
+
+if (page==1){
+    searchResult.innerHTML = "";
+}
 
 const results = data.results;
 
