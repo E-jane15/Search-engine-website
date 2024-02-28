b2178922589963697ec9201be6277fe12dc28b2f
E-jane15 <janeebaimay@gmail.com>
2024-02-28 22:10:40 +0100

    linked API correctly using back ticks and completed search engine

--- script.js
+++ script.js
@@ -6,20 +6,21 @@ const searchResult = document.getElementById("search-result");
 const showMoreBtn = document.getElementById("show-more-btn");
 
 
-let keyword = "";
+let keyword = ""; 
 let page = 1;
 
 async function searchImages(){
     keyword = searchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12';
-const response = await fetch(url);
-const data = await response.json();
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=12`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data); 
+
+    const results = data.results;
 
 if (page==1){
     searchResult.innerHTML = "";
 }
-
-const results = data.results;
 
 results.map((result) => {
    const image = document.createElement("img");
